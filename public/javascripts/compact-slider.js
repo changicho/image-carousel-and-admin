7396583c8bc69a8c0c28ff270e8321571f53d5b2
eunuch74 <eunuch74@gmail.com>
2019-09-17 19:17:09 +0900

    fix : make slide start at 0 index
    슬라이드가 0 번째 인덱스부터 시작하게 수정
    버튼 눌렀을 때, transition 이 끝날 떄까지 대기하게 오류 처리
    node 서버를 띄웠을 떄 /ver1 로 접근 가능하게 변경

--- public/javascripts/compact-slider.js
+++ public/javascripts/compact-slider.js
@@ -1,3 +1,6 @@
+/**
+ *
+ */
 class compact_slider {
   constructor() {
     this.div_slider = document.querySelector(".compact-slider");
@@ -16,7 +19,22 @@ class compact_slider {
 
     this.fill_axis_list(this.slide_list.length);
     this.fill_event();
+    this.set_first_slide();
   }
+
+  set_first_slide() {
+    let current_index = parseInt(this.slide_list.length / 2);
+    this.current_index = current_index;
+
+    Array.from(this.slide_list).reduce((pre, cur) => {
+      cur.style.left = `-${this.slide_width * this.current_index}px`;
+    }, []);
+
+    for (let i = 0; i < current_index; i++) {
+      this.move_prev();
+    }
+  }
+
   fill_axis_list(size) {
     for (let i = 0; i < size; i++) {
       this.axis_list.push(this.start_point);
@@ -25,53 +43,87 @@ class compact_slider {
   fill_event() {
     this.fill_button_event();
     this.fill_interval_event();
+    this.fill_transitionend_event();
   }
   fill_button_event() {
-    let button_prev = this.div_slider.querySelector("#button_prev");
-    let button_next = this.div_slider.querySelector("#button_next");
+    this.animation_end = true;
+    let button_prev = this.div_slider.querySelector("#button-prev");
+    let button_next = this.div_slider.querySelector("#button-next");
 
     button_prev.addEventListener("click", () => {
-      this.move_prev();
+      if (this.animation_end) {
+        this.animation_end = false;
+        this.move_prev();
+      }
     });
     button_next.addEventListener("click", () => {
-      this.move_next();
+      if (this.animation_end) {
+        this.animation_end = false;
+        this.move_next();
+      }
     });
   }
 
   fill_interval_event() {
     this.interval = setInterval(() => {
       this.move_next();
-    }, 4000);
+    }, 3000);
   }
 
+  fill_transitionend_event() {
+    this.slide_list[0].addEventListener("transitionend", () => {
+      this.animation_end = true;
+    });
+  }
   move_prev() {
     let slide_width = this.slide_width;
+
     Array.from(this.slide_list).reduce((pre, cur, index) => {
       this.axis_list[index] += slide_width;
+
       if (this.axis_list[index] + index * slide_width >= this.end_point) {
         this.axis_list[index] = -index * slide_width;
         cur.style.zIndex = -100;
       } else {
         cur.style.zIndex = 1;
       }
+
       cur.style.transform = `translate(${this.axis_list[index]}px, 0)`;
     }, []);
-    clearInterval(this.interval);
-    this.fill_interval_event();
+
+    this.current_index =
+      (this.current_index - 1 + this.slide_list.length) %
+      this.slide_list.length;
+
+    this.reset_interval();
   }
   move_next() {
     let slide_width = this.slide_width;
+
     Array.from(this.slide_list).reduce((pre, cur, index) => {
       this.axis_list[index] -= slide_width;
+
       if (this.axis_list[index] + index * slide_width < this.start_point) {
         this.axis_list[index] = this.end_point - (index + 1) * slide_width;
         cur.style.zIndex = -100;
       } else {
         cur.style.zIndex = 1;
       }
+
       cur.style.transform = `translate(${this.axis_list[index]}px, 0)`;
     }, []);
+
+    this.current_index =
+      (this.current_index + 1 + this.slide_list.length) %
+      this.slide_list.length;
+
+    this.reset_interval();
+  }
+
+  reset_interval() {
     clearInterval(this.interval);
     this.fill_interval_event();
   }
 }
+
+new compact_slider();
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index.html');
 });
 
+router.get('/ver1', function(req, res, next) {
+  res.render('carousel_v1.html');
+});
+
 module.exports = router;
