3b6820a1fa1e027efff04b4a387436432c7651f3
eunuch74 <eunuch74@gmail.com>
2019-09-18 17:11:58 +0900

    feat : make card-ui upper upside
    card ui 상단부분 제작
    card-ui_v1.html 파일로 확인 가능
    card-container.js로 모듈화.
    줌인, 줌아웃, 마크업 조건 완료.
    indicator 클릭 시 div tag 의 data-set 에 있는 index 를 이용해 콘솔에 출력
    
    simple slide 에 indicator 이동 기능 추가.

---
+++ public/javascripts/card-container.js
@@ -0,0 +1,66 @@
+class card_container {
+  constructor() {
+    this.card_container = document.querySelector(".card-container");
+    this.card_list = this.card_container.querySelectorAll(".card");
+
+    this.bind_event();
+  }
+  bind_event() {
+    this.bind_card_event();
+    this.bind_indicator_event();
+  }
+  bind_card_event() {
+    Array.from(this.card_list).reduce((pre, cur) => {
+      cur.addEventListener("click", evt => {
+        this.fucus_out_all_card();
+
+        let current_card_content = evt.currentTarget.querySelector(
+          ".card-content"
+        );
+        let current_slide_indicator = evt.currentTarget.querySelector(
+          ".slide-indicator"
+        );
+        let current_first_indicator = current_slide_indicator.querySelector(
+          ".indicator"
+        );
+
+        current_card_content.classList.add("focus");
+        current_slide_indicator.classList.remove("hidden");
+
+        current_first_indicator.click();
+      });
+    }, []);
+  }
+  bind_indicator_event() {
+    this.card_container.addEventListener("click", evt => {
+      let target_type = evt.target.classList[0];
+
+      if (target_type === "indicator") {
+        console.log(evt.target.dataset.page);
+        this.unselect_all_indicator();
+        evt.target.classList.add("selected");
+        return;
+      }
+    });
+  }
+  fucus_out_all_card() {
+    let card_content_list = this.card_container.querySelectorAll(
+      ".card-content"
+    );
+
+    Array.from(card_content_list).reduce((pre, cur) => {
+      cur.classList.remove("focus");
+      let slide_indicator = cur.parentNode.querySelector(".slide-indicator");
+      slide_indicator.classList.add("hidden");
+    }, []);
+  }
+  unselect_all_indicator() {
+    let indicator_list = this.card_container.querySelectorAll(".indicator");
+
+    Array.from(indicator_list).reduce((pre, cur) => {
+      cur.classList.remove("selected");
+    }, []);
+  }
+}
+
+new card_container();
