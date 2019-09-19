32bab823af71cf9d5293927f1116bc083c8b4e99
eunuch74 <eunuch74@gmail.com>
2019-09-20 01:52:47 +0900

    feat : add 4xx, 5xx status error handling
    fetch로 요청할 때 400, 500번대 에러가 발생할 시 처리하는 코드 추가.
    에러 코드 확인은 response.statas를 100으로 나눈 몫으로 판별

--- public/javascripts/fetch.js
+++ public/javascripts/fetch.js
@@ -6,14 +6,26 @@ fetch("/api/get_data", {
   method: "POST"
 })
   .then(function(response) {
-    return response.json();
+    if (response.status === 200 || response.status === 201) {
+      // 성공을 알리는 HTTP 상태 코드면
+      return response.json();
+    } else if (response.status / 100 == 4) {
+      // 400번대 에러
+      console.error(response.statusText);
+    } else if (response.status / 100 == 5) {
+      // 500번대 에러
+      console.error(response.statusText);
+    }
   })
   .then(function(myJson) {
+    if(myJson === undefined){
+      return;
+    }
     json_data = myJson;
 
     slide_data_card = json_data.slide;
     slide_data_second = json_data.card;
-  })
-  .then(function(){
+
     new main_script();
   })
+  .catch(err => console.error(err));
