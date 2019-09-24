2731aa70e1410f1899175e979a0957dffff0e425
eunuch74 <eunuch74@gmail.com>
2019-09-24 16:37:37 +0900

    feat : add admin url to router
    라우터에 adminpage로 이동 추가

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,16 @@ var express = require("express");
 var router = express.Router();
 var fs = require("fs");
 
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "amazon_api",
+  password: "pass1234",
+  port: 3306,
+  database: "amazon"
+});
 
-
-// console.log(data);
+connection.connect();
 
 /* GET users listing. */
 router.get("/", function(req, res, next) {
@@ -14,6 +21,22 @@ router.get("/", function(req, res, next) {
 router.post("/get_data", function(req, res, next) {
   fs.readFile("./public/data/data.json", "utf8", function(err, contents) {
     res.send(contents);
+  });
+});
+
+router.post("/check_id", function(req, res, next) {
+  connection.query("select * from user where id='hello'", (err, rows, fields) => {
+    let json;
+
+    if (!err) {
+      stringfy = JSON.stringify(rows);
+      json = JSON.parse(stringfy);
+
+      console.log(json);
+    } else {
+      console.log("Error while performing Query.", err);
+    }
+    res.send(json);
   });
 });
 
