44a8c8d440fcba05b21efc4fe7d7779c10ed40e5
eunuch74 <eunuch74@gmail.com>
2019-09-24 18:40:29 +0900

    feat : add get data from database
    api와 admin api를 분리.
    각 테이블의 모든 데이터를 뽑아오는 요청 추가
    기존에 만든 id, password 체크 기능 옮김

--- routes/admin.js
+++ routes/admin.js
@@ -1,11 +1,69 @@
 var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get("/", function(req, res, next) {
-  res.render("admin-login.html");
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+	host: "localhost",
+	user: "amazon_api",
+	password: "pass1234",
+	port: 3306,
+	database: "amazon",
 });
 
+connection.connect();
 
+/* GET home page. */
+router.get("/", function(req, res, next) {
+	res.render("admin-login.html");
+});
+
+router.post("/check_id", function(req, res, next) {
+	connection.query(
+		`select * from user where id='${req.body.id || "EMPTY"}';`,
+		(err, rows, fields) => {
+			if (err) {
+				console.log("Error while performing Query.", err);
+				res.send("error");
+			}
+			let is_match = false;
+
+			let stringfy = JSON.stringify(rows);
+			let json = JSON.parse(stringfy);
+
+			if (req.body.password == json[0].password) {
+				console.log("match");
+				is_match = true;
+			} else {
+				console.log("not match");
+			}
+
+			res.send({ data: is_match });
+		}
+	);
+});
+
+router.post("/get_downside_slide", function(req, res, next) {
+	connection.query("select * from downside_slide", (err, rows, fields) => {
+		let stringfy = JSON.stringify(rows);
+		let json = JSON.parse(stringfy);
+		res.send({ data: json });
+	});
+});
+
+router.post("/get_upside_card", function(req, res, next) {
+	connection.query("select * from upside_card", (err, rows, fields) => {
+		let stringfy = JSON.stringify(rows);
+		let json = JSON.parse(stringfy);
+		res.send({ data: json });
+	});
+});
+
+router.post("/get_upside_slide", function(req, res, next) {
+	connection.query("select * from upside_slide", (err, rows, fields) => {
+		let stringfy = JSON.stringify(rows);
+		let json = JSON.parse(stringfy);
+		res.send({ data: json });
+	});
+});
 
 module.exports = router;
--- routes/api.js
+++ routes/api.js
@@ -2,17 +2,6 @@ var express = require("express");
 var router = express.Router();
 var fs = require("fs");
 
-var mysql = require("mysql");
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "amazon_api",
-  password: "pass1234",
-  port: 3306,
-  database: "amazon"
-});
-
-connection.connect();
-
 /* GET users listing. */
 router.get("/", function(req, res, next) {
   res.send("respond with a resource");
@@ -21,22 +10,6 @@ router.get("/", function(req, res, next) {
 router.post("/get_data", function(req, res, next) {
   fs.readFile("./public/data/data.json", "utf8", function(err, contents) {
     res.send(contents);
-  });
-});
-
-router.post("/check_id", function(req, res, next) {
-  connection.query("select * from user where id='hello'", (err, rows, fields) => {
-    let json;
-
-    if (!err) {
-      stringfy = JSON.stringify(rows);
-      json = JSON.parse(stringfy);
-
-      console.log(json);
-    } else {
-      console.log("Error while performing Query.", err);
-    }
-    res.send(json);
   });
 });
 
